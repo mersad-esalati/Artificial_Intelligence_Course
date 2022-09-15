e4dbfd276048ae7cbc6dc3a2d36922b9a12800db
mersad-esalati <mersad.esalati@gmail.com>
2022-09-15 14:55:00 +0430

    upload project

---
+++ Blind_Huristic_Search/State.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * State interface from which problem states inherit. Defines a method to check
+ * if the current state is a goal, generate successors, and find the cost to
+ * come to the current state.
+ *
+ */
+public interface State {
+    // determine if current state is goal
+    boolean isGoal();
+
+    // generate successors to the current state
+    ArrayList<State> genSuccessors();
+
+    // determine cost from initial state to THIS state
+    double findCost();
+
+    // print the current state
+    void printState();
+
+    // compare the actual state data
+    boolean equals(State s);
+}
